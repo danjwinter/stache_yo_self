5a8a216483c7024c9111a8b1cc67c605f55c781c
Dan Winter <dan.j.winter@gmail.com>
2016-03-07 12:59:16 -0700

    remove path and url options from production.rb paperclip defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Rails.application.configure do
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-  :url =>':s3_domain_url',
-  :path => '/:class/:attachment/:id_partition/:style/:filename',
+  # :url =>':s3_domain_url',
+  # :path => '/:class/:attachment/:id_partition/:style/:filename',
 }
   # Settings specified here will take precedence over those in config/application.rb.
 
