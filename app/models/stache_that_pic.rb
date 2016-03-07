1f6f10ac0da456552bf9d9b848d2720e0c203db2
Dan Winter <dan.j.winter@gmail.com>
2016-03-07 15:32:45 -0700

    add puts to try to debug heroku paperclip issue

--- app/models/stache_that_pic.rb
+++ app/models/stache_that_pic.rb
@@ -19,16 +19,31 @@ class StacheThatPic
 
   def self.create_image(mustache_request)
     pic = mustache_request.user_info.image_url
+    # file = File.open('dan.jpg', 'wb') do |fo|
+    #   fo.write open(pic).read
+    # end
     # binding.pry
+    puts uri_pic = URI.parse(pic)
 
-    file = Tempfile.new ['', ".#{pic}"]
-file.binmode # note that our tempfile must be in binary mode
-file.write open(image_url).read
-file.rewind
-file
-
-    mustache_request.original_user_image = file
-    mustache_request.save
+    # tempfile = Net::HTTP.start(uri_pic.host, uri_pic.port) do |http|
+    #   resp = http.get(uri_pic.path)
+    #   file = Tempfile.new('foo', Dir.tmpdir, 'wb+')
+    #   file.binmode
+    #   file.write(resp.body)
+    #   file.flush
+    #   file
+    # end
+ #    signature = Paperclip.io_adapters.for(image)
+ # base_name = File.basename(image_name,File.extname(image_name))
+ # signature.original_filename = "#{base_name}.jpg"
+ # media_img = Media::Image.new()
+ # media_img.image = signature
+ # media_img.company_id = current_company_id
+ # media_img.type = cat
+ # media_img.save
+    # binding.pry
+    puts mustache_request.original_user_image = uri_pic
+    puts mustache_request.save
     magick_pic = Magick::Image.read(mustache_request.original_user_image.url).first
     sc = StacheCalculation.new(mustache_request.face_location)
     sized_magick_pic = magick_pic.resize_to_fill(400,400)
