fb53188109d809d129a7cdd6a627e1d5403e45d1
Dan Winter <dan.j.winter@gmail.com>
2018-09-14 17:32:33 -0600

    face location off

--- app/services/face_plus_plus_service.rb
+++ app/services/face_plus_plus_service.rb
@@ -45,6 +45,9 @@ class FacePlusPlusService
                                           mouth_right_x: json_response[:faces][0][:landmark][:mouth_right_corner][:x],
                                           mouth_right_y: json_response[:faces][0][:landmark][:mouth_right_corner][:y],
                                           nose_y: json_response[:faces][0][:landmark][:nose_tip][:y])
+      puts "this is the face locatoin we think we found:"
+      puts mustache_request.face_location
+      mustache_request.face_location
     end
   end
 
